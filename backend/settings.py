06f08200acacc160804decd352d111df8485c127
Bazil <king.bazil.khan@gmail.com>
2025-03-01 23:03:01 -0500

    adds cors middleware

--- backend/settings.py
+++ backend/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -154,3 +155,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = False  # 🚨 Keep this False for security
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # 👈 Allow React frontend
+]
