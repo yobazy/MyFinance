a73b8988abaec59135d30f89f3b035d26c620ebf
Bazil <bazil.khan@urbaneyes.ai>
2025-03-02 00:35:18 -0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -5,12 +5,36 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
+        
 class Transaction(models.Model):
     date = models.DateField()
-    description = models.CharField(max_length=255)
+    description = models.TextField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)  # ✅ Fixed
+    source = models.CharField(max_length=10)
+    merchant = models.TextField(null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.date} - {self.description} - ${self.amount}"
+    class Meta:
+        db_table = 'all_transactions'
+
+class TDTransaction(models.Model):
+    date = models.DateField()
+    charge_name = models.TextField()
+    credit_amt = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    debit_amt = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'td'
+
+class AmexTransaction(models.Model):
+    date = models.DateField()
+    date_processed = models.DateField()
+    description = models.TextField()
+    cardmember = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    commission = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    exc_rate = models.DecimalField(max_digits=10, decimal_places=4, null=True, blank=True)
+    merchant = models.TextField()
+
+    class Meta:
+        db_table = 'amex'
